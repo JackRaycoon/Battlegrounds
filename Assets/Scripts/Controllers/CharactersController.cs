cfe92a84baad21930d21cafe1defa0c0b18ee47e
JackRaycoon <jack.o.raycoon@gmail.com>
2025-05-27 15:07:14 +0700

    UI для способности

--- Assets/Scripts/Controllers/CharactersController.cs
+++ Assets/Scripts/Controllers/CharactersController.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 public class CharactersController : MonoBehaviour
 {
    public Sprite BOBSprite;
-   public Image EnemyImage, PlayerImage;
-   public TextMeshProUGUI hpText;
+   public Image EnemyImage, PlayerImage, AbilityImage, AbilityImageBig;
+   public TextMeshProUGUI hpText, costAbilityText, costAbilityTextBig, nameBig, descriptionBig;
 
    public GameController gameController;
    public EnemyDataController enemyDataController;
@@ -21,7 +21,14 @@ public class CharactersController : MonoBehaviour
    {
       EnemyImage.sprite = gameController.isFightNow ? enemyDataController.dataOfNextEnemies.spriteHero : BOBSprite ;
       PlayerImage.sprite = PlayerData.Instance.character.data.sprite;
+      AbilityImage.sprite = PlayerData.Instance.character.ability.data.spriteArt;
+      AbilityImageBig.sprite = PlayerData.Instance.character.ability.data.spriteArt;
 
+      costAbilityText.text = PlayerData.Instance.character.ability.data.cost.ToString();
       hpText.text = PlayerData.Instance.cur_hp.ToString();
+
+      costAbilityTextBig.text = PlayerData.Instance.character.ability.data.cost.ToString();
+      nameBig.text = PlayerData.Instance.character.ability.data.name;
+      descriptionBig.text = PlayerData.Instance.character.ability.data.description;
    }
 }
--- Assets/Scripts/Data/Character.cs
+++ Assets/Scripts/Data/Character.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class Character
 {
    public CharacterSO data;
+   public Spell ability;
 
    public Character(string name)
    {
       data = Resources.Load<CharacterSO>($"Cards/Characters/{name}");
+      ability = new(data.ability);
    }
    public Character(CharacterSO data)
    {
       this.data = data;
+      ability = new(data.ability);
    }
 }
